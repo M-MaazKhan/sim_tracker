67e14488b68bd494f1f686d3a87b80827453d454
Your Name <muhammadmaazkhan3.0.2@email.com>
2026-07-07 00:30:31 -0700

    Add statistics section and update HTML structure for improved content presentation

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function searchItem() {
     alert("Search icon clicked!");
 }
+     function trackWhatsappClick(e, url) {
+    e.preventDefault();
+    fetch('track_whatsapp.php')
+        .then(() => {
+            window.open(url, '_blank');
+        })
+        .catch(() => {
+            window.open(url, '_blank');
+        });
+}
+ 
